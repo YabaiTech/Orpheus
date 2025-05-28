b6c82d9124ec7660833f0571ed28fa15aa2bd6b6
M-H-MARUF-1903 <md.muktadirul.haque.maruf@gmail.com>
2025-05-28 12:21:53 +0600

    Revert "Change the greeting text to 'Hello Java'"
    This reverts commit ee07495e2c214f912cd28ad6c579c4dd693f4d4e.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,8 @@ package org.example;
 
 public class App {
   public String getGreeting() {
-    return "Hello world!";
+    // This is a specific comment
+    return "Hello Project!";
   }
 
   public static void main(String[] args) {
